bc850782abe1ba850c2f969aae95dfa42181f641
Furkan Akif ISLEK <furkanakif2702@gmail.com>
2022-07-02 23:20:33 +0300

    components list ve form componentları yazıldı.

---
+++ src/components/Contacts/Form/index.js
@@ -0,0 +1,56 @@
+import {useState} from 'react'
+
+
+const initialFormValues = ({ fullname: "", phone_number:""})
+
+function Form({ addContact, contacts }) {
+    const [form,setForm] = useState(initialFormValues);
+
+    const onChangeInput = (e) => {
+        setForm({...form, [e.target.name]: e.target.value});
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        if (form.fullname === "" || form.phone_number === "") {
+           return false; 
+        }
+
+        addContact([...contacts,form]);
+
+        setForm(initialFormValues)
+    }
+
+  return (
+
+    <form onSubmit={onSubmit}>
+        <div>
+            <div>
+                <input 
+                name='fullname' 
+                placeholder='Full Name' 
+                value={form.fullname}
+                onChange={onChangeInput} />
+            </div>
+            
+            <div>
+                <input 
+                name='phone_number' 
+                placeholder='Phone Number' 
+                value={form.phone_number}
+                onChange={onChangeInput}/>
+            </div>
+
+            <div>
+                <button>Add</button>
+            </div>
+        </div>
+        
+        
+    </form>
+    
+  )
+}
+
+export default Form;
---
+++ src/components/Contacts/List/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function List( {contacts} ) {
+  return (
+    <div>
+      <ul>
+        {contacts.map((contact, i) => (
+          <li key={i}> {contact.fullname} {contact.phone_number} </li>
+        ))}
+        
+      </ul>
+    </div>
+  );
+}
+
+export default List;
